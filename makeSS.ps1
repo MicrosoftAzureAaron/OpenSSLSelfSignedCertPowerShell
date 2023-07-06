de6446c4e927f190385f047866111d430dd0e8cb
Azure Aaron <aarosanders@microsoft.com>
2023-07-06 18:38:01 -0500

    updated user password input

--- makeSS.ps1
+++ makeSS.ps1
@@ -7,20 +7,20 @@ if (!(Get-Command openssl -ErrorAction SilentlyContinue)) {
 }
 
 # Ask user for parameters
-$certName = Read-Host "Enter Site/Leaf/Server certificate name, example.com wild card will be included"
-# $RootCertPassword = Read-Host "Enter Root Certificate password" -AsSecureString
-# $LeafCertPassword = Read-Host "Enter Leaf Certificate password" -AsSecureString
+$certName = Read-Host "Enter Site/Leaf/Server certificate name, example.com"
+$RootCertPassword = Read-Host "Enter Root Private Key password" -AsSecureString
+$LeafCertPassword = Read-Host "Enter Leaf Private Key password" -AsSecureString
 $certDays = 366
-$LeafCertPassword = $RootCertPassword = "1234"
+#$LeafCertPassword = $RootCertPassword = "1234"
 
 # Create Root Certificate and Root Key
 openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -days $certDays -nodes -keyout SelfSignedRoot.key -out SelfSignedRoot.crt -subj "/CN=SelfSignedRoot.com" -addext "subjectAltName = DNS:SelfSignedRoot.com,DNS:*SelfSignedRoot.com" -passout pass:$RootCertPassword
 
 $txt = "subjectAltName = DNS:"+$certName+",DNS:*"+$certname
-$cn = "/CN=*"+$certName
+$cn = "/CN="+$certName
 # Create the leaf certifite and key
 openssl req -new -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -keyout SelfSignedLeaf.key -out SelfSignedLeaf.csr -subj $cn -addext $txt -passout pass:$LeafCertPassword
-openssl x509 -req -in SelfSignedLeaf.csr -CA SelfSignedRoot.crt -CAkey SelfSignedRoot.key -CAcreateserial -out SelfSignedLeaf.crt -days $certDays -passin pass:$IntermediateCertPassword
+openssl x509 -req -in SelfSignedLeaf.csr -CA SelfSignedRoot.crt -CAkey SelfSignedRoot.key -CAcreateserial -out SelfSignedLeaf.crt -days $certDays -passin pass:$RootCertPassword
 
 # Export server certificate and key
 openssl pkcs12 -export -out SelfSignedLeaf.pfx -inkey SelfSignedLeaf.key -in SelfSignedLeaf.crt -certfile SelfSignedRoot.crt -passout pass:$LeafCertPassword -passin pass:$LeafCertPassword
