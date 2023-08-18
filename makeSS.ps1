988edd71b3b77ce82b23895833c2099b6673499b
Azure Aaron <aarosanders@microsoft.com>
2023-08-18 16:04:31 -0500

    prettier?

--- makeSS.ps1
+++ makeSS.ps1
@@ -2,8 +2,8 @@
 if (!(Get-Command openssl -ErrorAction SilentlyContinue)) {
     Write-Host "Installing OpenSSL"
     # Download and install OpenSSL
-    Invoke-WebRequest -Uri "https://slproweb.com/download/Win64OpenSSL_Light-3_1_2.exe" -OutFile "OpenSSL.exe"
-    Start-Process -FilePath ".\OpenSSL.exe" -Wait
+    Invoke-WebRequest -Uri "https://slproweb.com/download/Win64OpenSSL_Light-3_1_2.exe" -OutFile "C:\Users\$env:USERNAME\Downloads\OpenSSL.exe"
+    Start-Process -FilePath "C:\Users\$env:USERNAME\Downloads\OpenSSL.exe" -Wait
     $env:Path += ";C:\Program Files\OpenSSL-Win64\bin"
 }
 
@@ -11,31 +11,33 @@ function CreateCNFTemplate {
     # Specify the file path
     $filePath = Join-Path -Path $PSScriptRoot -ChildPath "cnftemplate.cnf"
     if (Test-Path $filePath -PathType Leaf) {
-       #Write-Host "The CNF template file exists."
-   } else { #manually create template
-       Add-Content -Path $filePath -Value "[ req ] `ndistinguished_name = req_distinguished_name `nextensions = v3_ca `nreq_extensions = v3_ca `nprompt = no `n[ v3_ca ] `nbasicConstraints = CA:TRUE `n[ req_distinguished_name ] `ncountryName = s1 `nstateOrProvinceName = s2 `norganizationName = s3 `ncommonName = s4"
-   }
+        #Write-Host "The CNF template file exists."
+    }
+    else {
+        #manually create template
+        Add-Content -Path $filePath -Value "[ req ] `ndistinguished_name = req_distinguished_name `nextensions = v3_ca `nreq_extensions = v3_ca `nprompt = no `n[ v3_ca ] `nbasicConstraints = CA:TRUE `n[ req_distinguished_name ] `ncountryName = s1 `nstateOrProvinceName = s2 `norganizationName = s3 `ncommonName = s4"
+    }
 }
 
 ############# create certificate
 function CreateCerts {
     $CommonName = Read-Host "Enter Root certificate name, [Root.com]"
-    if($CommonName -match "^\s*$") {$CommonName = "Root.com"}
+    if ($CommonName -match "^\s*$") { $CommonName = "Root.com" }
 
     $countryName = Read-Host "Enter the Country, [US]"
-    if($countryName -match "^\s*$") {$countryName = "US" }
+    if ($countryName -match "^\s*$") { $countryName = "US" }
 
     $stateOrProvinceName = Read-Host "Enter the State or Providence, [TX]"
-    if($stateOrProvinceName -match "^\s*$") {$stateOrProvinceName = "TX"}
+    if ($stateOrProvinceName -match "^\s*$") { $stateOrProvinceName = "TX" }
 
     $organizationName = Read-Host "Enter the Orginization Name, [SelfSigned]"
-    if($organizationName -match "^\s*$") {$organizationName = "SelfSigned"}
+    if ($organizationName -match "^\s*$") { $organizationName = "SelfSigned" }
 
     $RootCertPassword = Read-Host "Enter Root Private Key password"
     $RootCertPassword = "pass:" + $RootCertPassword
 
     $CertDays = Read-Host "Enter days the certificate is valid for [365]"
-    if($CertDays -match "^\s*$") {$CertDays = 365}
+    if ($CertDays -match "^\s*$") { $CertDays = 365 }
 
     #create Root.cnf
     $FN = "Root.cnf"
@@ -54,7 +56,7 @@ function CreateCerts {
     openssl ecparam -out Leaf.key -name prime256v1 -genkey
     $CommonName = ""
     $CommonName = Read-Host "Enter Site/Leaf/Server certificate name, Leaf.com"
-    if($CommonName -match "^\s*$") {$CommonName = "Leaf.com" }
+    if ($CommonName -match "^\s*$") { $CommonName = "Leaf.com" }
 
     $LeafCertPassword = Read-Host "Enter Leaf Private Key password"
     $LeafCertPassword = "pass:" + $LeafCertPassword
@@ -67,11 +69,12 @@ function CreateCerts {
     $SAN = Read-Host "Enter the SANs for the certificate, []"
     $SAN = $SAN.ToLower()
 
-    if($SAN -match "^\s*$") { #no SAN entered :(
-        $SAN="DNS:" + $CommonName.ToLower()
+    if ($SAN -match "^\s*$") {
+        #no SAN entered :(
+        $SAN = "DNS:" + $CommonName.ToLower()
     } #could ignore SANs if not entered
     else {
-         #add 'DNS:' to the list
+        #add 'DNS:' to the list
         $SAN = ($SAN -split ',') -join ',DNS:'
         $SAN = 'DNS:' + $SAN  # Add 'DNS:' to the beginning
         #Write-Host "Multiple SANS "
@@ -84,7 +87,7 @@ function CreateCerts {
     }  
     else {
         # Add the common name to the end of the SAN list
-        $SAN = $SAN +",DNS:" + $CommonName.ToLower()
+        $SAN = $SAN + ",DNS:" + $CommonName.ToLower()
         Write-Host "`nHad to add the common name to the SAN list`n" #$elements
     }
 
@@ -149,7 +152,7 @@ function CreateV3 {
     $filePath = Join-Path -Path $PSScriptRoot -ChildPath "v3.txt"
 
     # Content to be written to the file
-    $content = "subjectAltName = "+$s1
+    $content = "subjectAltName = " + $s1
     $content | Set-Content -Path $filePath
 
     #add the last line
@@ -159,69 +162,69 @@ function CreateV3 {
 function Get-OpenSSL {
     try {	
         # Download and install OpenSSL
-        Invoke-WebRequest -Uri "https://slproweb.com/download/Win64OpenSSL_Light-3_1_2.exe" -OutFile "OpenSSL.exe"
-        Start-Process -FilePath ".\OpenSSL.exe" -Wait
+        Invoke-WebRequest -Uri "https://slproweb.com/download/Win64OpenSSL_Light-3_1_2.exe" -OutFile "C:\Users\$env:USERNAME\Downloads\OpenSSL.exe"
+        Start-Process -FilePath "C:\Users\$env:USERNAME\Downloads\OpenSSL.exe" -Wait
         $env:Path += ";C:\Program Files\OpenSSL-Win64\bin"
     }
     catch {
         Write-Host "Check which link the script is trying to download. Attempting to open the website for you."
-        start https://slproweb.com/products/Win32OpenSSL.html
+        Start-Process https://slproweb.com/products/Win32OpenSSL.html
     }
 }
 
 #menu here
 function Show-Menu {
-	Clear-Host
-	Write-Host "================ Self Signed Certificate ================"
-	Write-Host "1: Press '1' Create Certificate "
-	Write-Host "2: Press '2' View Local PFX"
-	Write-Host "3: Press '3' Install OpenSSL"
-	Write-Host "4: Press '4' Open the SLBWebsite for OpenSSL"
-	#Write-Host "5: Press '5' "
-	#Write-Host "6: Press '6' "
-	#Write-Host "9: Press '9' "
-	#Write-Host "0: Press '0' "
-	Write-Host "Q: Press 'Q' to quit."
+    Clear-Host
+    Write-Host "================ Self Signed Certificate ================"
+    Write-Host "1: Press '1' Create Certificate "
+    Write-Host "2: Press '2' View Local PFX"
+    Write-Host "3: Press '3' Install OpenSSL"
+    Write-Host "4: Press '4' Open the SLBWebsite for OpenSSL"
+    #Write-Host "5: Press '5' "
+    #Write-Host "6: Press '6' "
+    #Write-Host "9: Press '9' "
+    #Write-Host "0: Press '0' "
+    Write-Host "Q: Press 'Q' to quit."
 }
 
 do {
-	Show-Menu
+    Show-Menu
     $S = Read-Host "Please make a selection"
 
     switch ($S) {
-		'1' {
+        '1' {
             #get the user input
             CreateCerts
             Pause
-		}
-		'2' {
+        }
+        '2' {
             #view the pfx
             openssl pkcs12 -in .\FullCertChain.pfx -nodes
             Pause
-		}
-		'3' {
+        }
+        '3' {
             Get-OpenSSL
             Pause
-		}
-		'4' {
-            start https://slproweb.com/products/Win32OpenSSL.html
-		}
-		'5' {
+        }
+        '4' {
+            Start-Process https://slproweb.com/products/Win32OpenSSL.html
+        }
+        '5' {
 
-		}
-		'6' {
+        }
+        '6' {
 
-		}
-		'9' {
+        }
+        '9' {
 
-		}
-		'q' {
-			try {	
-			}
-			catch {
-				Write-Host "Error"
-			}
-		}
-	}
+        }
+        'q' {
+            try {	
+            }
+            catch {
+                Write-Host "Error"
+            }
+        }
+    }
 }
 until ($S -eq 'q')
